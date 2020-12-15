34b229b3c01885dbab116b4f8e82efec02941b8e
anisa1499 <55905825+anisa1499@users.noreply.github.com>
2020-12-15 23:16:16 +0700

    Add files via upload

---
+++ kepribadiansiswa/footer.php
@@ -0,0 +1,11 @@
+<footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        &copy; 2017 Klasifikasi Naive Bayes | Distributed by <a href="https://blogbugabagi.blogspot.com" target="_blank" rel="noopener noreferrer">BlogBugaBagi</a>
+                    </div>
+
+                </div>
+            </div>
+        </footer>
+        <!-- FOOTER SECTION END-->
